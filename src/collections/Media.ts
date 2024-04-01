d1c68870bd5169f128dfbddc64d14a3c39a6f981
Tsemach Hadad <tsemachh@gmail.com>
2024-04-01 16:31:14 +0300

    Media chanegs

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -6,6 +6,31 @@ export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
     staticDir: path.resolve(__dirname, '../../../media'),
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+      {
+        name: 'card',
+        width: 768,
+        height: 1024,
+        position: 'centre',
+      },
+      {
+        name: 'tablet',
+        width: 1024,
+        // By specifying `undefined` or leaving a height undefined,
+        // the image will be sized to a certain width,
+        // but it will retain its original aspect ratio
+        // and calculate a height automatically.
+        height: undefined,
+        position: 'centre',
+      },
+    ],
+    adminThumbnail: 'thumbnail',
   },
   access: {
     read: () => true,
@@ -18,12 +43,10 @@ export const Media: CollectionConfig = {
     },
     {
       name: 'caption',
-      type: 'richText',
-      editor: slateEditor({
-        admin: {
-          elements: ['link'],
-        },
-      }),
+      type: 'richText'
     },
   ],
+  admin: {
+    listSearchableFields: ['filename', 'alt', 'caption']
+  }
 }
